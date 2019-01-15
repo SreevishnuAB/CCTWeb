9156853c08f6ad1f3da363b56d19dcacd2b1fb32
SreevishnuAB <sreeab28@gmail.com>
2019-01-16 03:47:20 +0530

    Minor error fixing

--- php/adddata.php
+++ php/adddata.php
@@ -1,10 +1,11 @@
 <?php
+  error_reporting(E_ALL);
   $req_body = file_get_contents("php://input");
   $req_data = json_decode($req_body);
-  $user = $req_body["user"];
-  $event = $req_body["event"];
-  $id = $req_body["id"];
-  $credits = $req_body["credits"];
+  $user = $req_body->user;
+  $event = $req_body->event;
+  $id = $req_body->id;
+  $credits = $req_body->credits;
   $dbserver="eu-cdbr-west-02.cleardb.net";
   $dbuser="b7c754d2844753";
   $dbpwd="0d754f7c";
@@ -13,7 +14,7 @@
   if($conn->connect_error)
     die("Connection failed: ".$conn->connect_error);
   $sql = "insert into studdata(id,event,credits,user) values('$id','$event','$credits','$user')";
-  $query->status="";
+  $query->status=$user;
   if ($conn->query($sql) === TRUE) {
     $query->status = "Entry added";
   }
